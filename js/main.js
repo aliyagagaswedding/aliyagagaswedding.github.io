3590eb6740b5774dbf023f4e21e41f859347c62e
aliyagagas <88587847+aliyagagaswedding@users.noreply.github.com>
2021-08-21 16:54:34 +0700

    update main.js
    update main.js

--- js/main.js
+++ js/main.js
@@ -232,7 +232,7 @@
   };
 
   // Set the date we're counting down to
-  var countDownDate = new Date("Sept 5, 2021 10:00:00").getTime();
+  var countDownDate = new Date("Aug 28, 2021 08:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
